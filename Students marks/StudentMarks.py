6f076ba2ba580cbbcd31b00eb2917cdded5b17b8
nadilHesara <nadilhesara@gmail.com>
2025-12-02 12:39:43 +0530

    Corrected some warnings in the dataframe

--- Students marks/StudentMarks.py
+++ Students marks/StudentMarks.py
@@ -2,8 +2,9 @@ import pandas as pd
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 from scipy import stats
+from pathlib import Path
 
-marks = pd.read_csv("Students marks\Student_Marks.csv")
+marks = pd.read_csv(Path("Students marks") / "Student_Marks.csv")
 
 
 X = marks[['number_courses', 'time_study']]
@@ -15,11 +16,17 @@ regr.fit(X,y)
 
 ##############predicted marks for some students#################
 
-Jim = regr.predict([[2, 5.321]])
-Kamal = regr.predict([[6, 20.534]])
-Nimal = regr.predict([[4, 7.432]])
-Haritha = regr.predict([[9, 5]])
-Gamunu = regr.predict([[5, 9.500]])
+pred_inputs = pd.DataFrame(
+    [[2, 5.321],
+     [6, 20.534],
+     [4, 7.432],
+     [9, 5.0],
+     [5, 9.5]],
+    columns=X.columns
+)
+
+preds = regr.predict(pred_inputs)
+Jim, Kamal, Nimal, Haritha, Gamunu = preds
 
 print(Jim, Kamal, Nimal, Haritha, Gamunu)
 
