772bd202005c642d84ea7743690990cc41bb020b
nadilHesara <nadilhesara@gmail.com>
2025-12-02 12:35:17 +0530

    Used a multiple linear model for predictions.

--- Students marks/StudentMarks.py
+++ Students marks/StudentMarks.py
@@ -1,7 +1,28 @@
 import pandas as pd
+from sklearn import linear_model
 import matplotlib.pyplot as plt
 from scipy import stats
 
-marks = pd.read_csv("Student_Marks.csv")
+marks = pd.read_csv("Students marks\Student_Marks.csv")
 
-print(marks)
+
+X = marks[['number_courses', 'time_study']]
+y = marks['Marks']
+
+regr = linear_model.LinearRegression()
+
+regr.fit(X,y)
+
+##############predicted marks for some students#################
+
+Jim = regr.predict([[2, 5.321]])
+Kamal = regr.predict([[6, 20.534]])
+Nimal = regr.predict([[4, 7.432]])
+Haritha = regr.predict([[9, 5]])
+Gamunu = regr.predict([[5, 9.500]])
+
+print(Jim, Kamal, Nimal, Haritha, Gamunu)
+
+################################################################
+
+
