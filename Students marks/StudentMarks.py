477cd3d0170f9616c4afd210828025f21f00b0f6
nadilHesara <nadilhesara@gmail.com>
2025-12-07 22:46:49 +0530

    Evaluating the model using metrics

--- Students marks/StudentMarks.py
+++ Students marks/StudentMarks.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import numpy as np
+
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score 
+
 import matplotlib.pyplot as plt
+
 from scipy import stats
+
 from pathlib import Path
+
 
 marks = pd.read_csv(Path("Students marks") / "Student_Marks.csv")
 
@@ -10,9 +18,32 @@ marks = pd.read_csv(Path("Students marks") / "Student_Marks.csv")
 X = marks[['number_courses', 'time_study']]
 y = marks['Marks']
 
-regr = linear_model.LinearRegression()
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42 )
 
-regr.fit(X,y)
+model = linear_model.LinearRegression()
+model.fit(X_train,y_train)
+
+y_pred = model.predict(X_test)
+
+
+#####################Evaluating the model#######################
+
+MAE = mean_absolute_error(y_test, y_pred)
+RMSE = np.sqrt(mean_squared_error(y_test, y_pred))
+R2 = r2_score(y_test, y_pred)
+
+print("MAE: ", MAE)
+print("RMSE: ", RMSE)
+print("R2: ", R2)
+
+
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Marks")
+plt.ylabel("Predicted Marks")
+plt.title("Actual vs Predicted Marks")
+plt.show()
+
+
 
 ##############predicted marks for some students#################
 
@@ -25,11 +56,14 @@ pred_inputs = pd.DataFrame(
     columns=X.columns
 )
 
-preds = regr.predict(pred_inputs)
+preds = model.predict(pred_inputs)
 Jim, Kamal, Nimal, Haritha, Gamunu = preds
 
 print(Jim, Kamal, Nimal, Haritha, Gamunu)
 
 ################################################################
+
+
+
 
 
